f5c3f6861fc5f6f0e3ca8ff00450ce2add4a9b50
“azmi-maz” <adfadhli1991@gmail.com>
2024-03-21 01:18:21 +0000

    User class attr set + get

--- src/main/java/org/group/project/classes/User.java
+++ src/main/java/org/group/project/classes/User.java
@@ -6,4 +6,48 @@ package org.group.project.classes;
  * @author azmi_maz
  */
 public class User {
+    protected String firstName;
+    protected String lastName;
+    protected String username;
+
+    /**
+     * This constructor creates a new user with basic data.
+     *
+     * @param firstName - First name of the user.
+     * @param lastName - Last name of the user.
+     * @param username - The username used to log in.
+     */
+    public User(String firstName, String lastName, String username) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+    }
+
+    /**
+     * Getter method to get the user first name.
+     *
+     * @return the first name of the user.
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Getter method to get the user last name.
+     *
+     * @return the last name of the user.
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Getter method to get the user's username.
+     *
+     * @return the username used by the user.
+     */
+    public String getUsername() {
+        return username;
+    }
+
 }
