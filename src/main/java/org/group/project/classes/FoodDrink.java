f308b930d5022f96bfb1bf18aeb90528d6a911ac
“azmi-maz” <adfadhli1991@gmail.com>
2024-03-21 10:51:39 +0000

    Edited FoodDrink class.

--- src/main/java/org/group/project/classes/FoodDrink.java
+++ src/main/java/org/group/project/classes/FoodDrink.java
@@ -1,8 +1,53 @@
 package org.group.project.classes;
 
 /**
- * This class
+ * This class handles the menu item to represent food or drink.
  *
+ * @author azmi_maz
  */
 public class FoodDrink {
+    private String itemName;
+    private String itemType;
+
+    private boolean isDailySpecial;
+
+
+    /**
+     * This constructor creates a new menu item.
+     *
+     * @param itemName - the name of the food or drink.
+     * @param itemType - the type of the item, either food or drink.
+     */
+    public FoodDrink(String itemName, String itemType) {
+        this.itemName = itemName;
+        this.itemType = itemType;
+    }
+
+    /**
+     * Getter method to get the item name.
+     *
+     * @return the name of the item.
+     */
+    public String getItemName() {
+        return itemName;
+    }
+
+    /**
+     * Getter method to get the item type.
+     *
+     * @return the type of the item.
+     */
+    public String getItemType() {
+        return itemType;
+    }
+
+    /**
+     * Getter method to check if the item is daily special or not.
+     *
+     * @return true if the item is daily special.
+     */
+    public boolean isItemDailySpecial() {
+        return isDailySpecial;
+    }
+
 }
