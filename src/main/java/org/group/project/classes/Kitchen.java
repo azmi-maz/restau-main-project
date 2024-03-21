c3dc59f5a6be30383972d97c2958e00e6446d9e3
“azmi-maz” <adfadhli1991@gmail.com>
2024-03-21 09:31:25 +0000

    Edited Kitchen class with specific getter/filter methods; Added FoodDrink class.

--- src/main/java/org/group/project/classes/Kitchen.java
+++ src/main/java/org/group/project/classes/Kitchen.java
@@ -1,5 +1,7 @@
 package org.group.project.classes;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 /**
@@ -9,5 +11,89 @@ import java.util.List;
  */
 public class Kitchen {
     private List<Order> orderTickets;
+
+    /**
+     * This constructor is default without any parameters.
+     */
+    public Kitchen() {}
+
+    /**
+     * Getter method to get all the order tickets ever existed from the kitchen.
+     *
+     * @return the list of all order tickets ever made.
+     */
+    public List<Order> getAllOrderTickets() {
+        return orderTickets;
+    }
+
+    /**
+     *
+     *
+     * @return
+     */
+    public List<Order> getPendingOrderTickets() {
+        // To code
+        return orderTickets;
+    }
+
+    /**
+     * This method adds a new order ticket to the kitchen.
+     *
+     * @param newOrder - New order ticket
+     * @return true if the ticket was added successfully.
+     */
+    public boolean addNewOrderTicket(Order newOrder) {
+        // to code
+        return true;
+    }
+
+    /**
+     *
+     *
+     * @param searchDate
+     * @return
+     */
+    public List<Order> getOrderListByDate(LocalDate searchDate) {
+        // to code
+        return orderTickets;
+    }
+
+    public List<Order> getOrderListByDateRange(LocalDate dateFrom,
+                                               LocalDate dateTo) {
+        // to code
+        return orderTickets;
+    }
+
+    public List<Order> getOrderListByDateAndTimeRange(LocalDate searchDate,
+                                                      LocalTime startTime,
+                                                      LocalTime endTime) {
+        // to code
+        return orderTickets;
+    }
+
+    public List<Order> getOrderListByStatus(String searchStatus) {
+        // to code
+        return orderTickets;
+    }
+
+    public List<Order> getOrderListByOrderType(String searchType) {
+        // to code
+        return orderTickets;
+    }
+
+    public List<FoodDrink> getDescendingListOfMostItemOrdered(int numberLimit) {
+        // to do
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
