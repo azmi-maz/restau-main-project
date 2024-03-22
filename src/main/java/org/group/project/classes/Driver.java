58225353755929a9b263c1a62905f03af8d5904e
“azmi-maz” <adfadhli1991@gmail.com>
2024-03-22 19:23:33 +0000

    Driver class attr + set + get

--- src/main/java/org/group/project/classes/Driver.java
+++ src/main/java/org/group/project/classes/Driver.java
@@ -1,16 +1,35 @@
 package org.group.project.classes;
 
 public class Driver extends Staff {
-
+    private boolean isAvailable;
 
     /**
      * The constructor to create new driver
      *
-     * @param firstName     - the first name of the staff.
-     * @param lastName      - the last name of the staff.
-     * @param username      - the username chosen by the staff.
+     * @param firstName - the first name of the staff.
+     * @param lastName  - the last name of the staff.
+     * @param username  - the username chosen by the staff.
      */
     public Driver(String firstName, String lastName, String username) {
         super(firstName, lastName, username, false);
+        this.isAvailable = true;
+    }
+
+    /**
+     * Getter method to get driver's free to do a round of deliveries.
+     *
+     * @return true if driver is available.
+     */
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    /**
+     * This method sets the driver to be available or not for deliveries.
+     *
+     * @param newStatus - true for available, false for out for deliveries.
+     */
+    public void setAvailability(boolean newStatus) {
+        isAvailable = newStatus;
     }
 }
