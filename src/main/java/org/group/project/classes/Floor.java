a9342e0704a05b3588d1c87a8f3250dfed0ee224
“azmi-maz” <adfadhli1991@gmail.com>
2024-03-22 21:50:46 +0000

    Added Floor class.

--- src/main/java/org/group/project/classes/Floor.java
+++ src/main/java/org/group/project/classes/Floor.java
@@ -19,15 +19,28 @@ public class Floor {
         tableBookings = new HashMap<>();
     }
 
+    /**
+     * Getter method to get the list of table reservations.
+     * @return the list of table reservations.
+     */
     public List<Table> getListOfTables() {
         // to check
         return (List<Table>) tableBookings.keySet();
     }
 
+    /**
+     * This method add new table to the tableBookings.
+     * @param newTable - new table to be added.
+     */
     public void addNewTable(Table newTable) {
         tableBookings.put(newTable, new ArrayList<>());
     }
 
+    /**
+     * This method adds new booking to the list of table reservations.
+     * @param booking - the new booking to be added.
+     * @return true if the booking was added successfully.
+     */
     public boolean addBookingToReservation(Booking booking) {
         // To check - this only handles one table preference, how about many
         // tables?
@@ -38,6 +51,11 @@ public class Floor {
             return true;
         }
         return false;
+    }
+
+    public List<Booking> getAllUniqueBookings() {
+
+        return new ArrayList<>();
     }
 
 
