0b0e4075ed120bf234aefaf825499709624d0f70
“azmi-maz” <adfadhli1991@gmail.com>
2024-03-25 02:57:58 +0000

    Edited LoginController to be more responsive and added main-header.css to aid in the UI.

--- src/main/java/org/group/project/Main.java
+++ src/main/java/org/group/project/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login" +
                 "-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 1400, 700);
         stage.setTitle("Cafe94 Restaurant");
         stage.setScene(scene);
         stage.show();
--- src/main/java/org/group/project/controller/LoginController.java
+++ src/main/java/org/group/project/controller/LoginController.java
@@ -3,6 +3,8 @@ package org.group.project.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import org.group.project.classes.User;
+import org.group.project.test.generators.CustomerGenerator;
 
 public class LoginController {
 
@@ -13,8 +15,31 @@ public class LoginController {
     private Label resultLabel;
 
     @FXML
-    protected void onLoginButtonClick() {
-        resultLabel.setText(loginUsername.getText());
+    protected void onLoginButtonClick() throws InterruptedException {
+        // This is for testing only!
+        User user = CustomerGenerator.createCustomer1();
+
+        if (loginUsername.getText().equalsIgnoreCase(user.getUsername())){
+            System.out.println(resultLabel.getStyleClass());
+            resultLabel.getStyleClass().removeAll("error");
+            resultLabel.getStyleClass().removeAll("success");
+            resultLabel.getStyleClass().removeAll("hidden-label");
+            resultLabel.getStyleClass().add("success");
+            System.out.println(resultLabel.getStyleClass());
+            resultLabel.setText("Login successful!");
+        } else {
+            resultLabel.setText("Login unsuccessful. Please try again.");
+//            resultLabel.getStylesheets()
+//            resultLabel.getStylesheets().add("");
+//            resultLabel.setVisible(false);
+//            System.out.println(resultLabel.getClass());
+            System.out.println(resultLabel.getStyleClass());
+            resultLabel.getStyleClass().removeAll("hidden-label");
+            resultLabel.getStyleClass().removeAll("success");
+            resultLabel.getStyleClass().removeAll("error");
+            resultLabel.getStyleClass().add("error");
+            System.out.println(resultLabel.getStyleClass());
+        }
     }
 
 }
