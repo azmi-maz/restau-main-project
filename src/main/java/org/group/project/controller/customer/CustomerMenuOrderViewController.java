22832dbda492a5e9c301b2b7a3c92b03a3b9a055
“azmi-maz” <adfadhli1991@gmail.com>
2024-03-30 15:44:49 +0000

    Checked all menu item png - all displayed ok.

--- src/main/java/org/group/project/controller/customer/CustomerMenuOrderViewController.java
+++ src/main/java/org/group/project/controller/customer/CustomerMenuOrderViewController.java
@@ -19,41 +19,51 @@ public class CustomerMenuOrderViewController {
     public void initialize() throws URISyntaxException {
 
         Image image1 = new Image(Main.class.getResource("images" +
-                "/menu/bloody-mary.png").toURI().toString());
+                "/menu/strip-steak-frite.png").toURI().toString()); // yes
         Image image2 = new Image(Main.class.getResource("images" +
-                "/menu/croquembouche.png").toURI().toString());
+                "/menu/stuffed-pork-tenderloin.png").toURI().toString()); // yes
         Image image3 = new Image(Main.class.getResource("images" +
-                "/menu/manhattan.png").toURI().toString());
+                "/menu/strip-steak-frite.png").toURI().toString()); // yes
+        Image image4 = new Image(Main.class.getResource("images" +
+                "/menu/stuffed-pork-tenderloin.png").toURI().toString()); // yes
 
         // Create ImageViews for each image
         ImageView imageView1 = new ImageView(image1);
-        imageView1.setFitWidth(200);
-        imageView1.setFitHeight(200);
-        ImageView imageView2 = new ImageView(image1);
-        imageView2.setFitWidth(450);
-        imageView2.setFitHeight(450);
+        imageView1.fitHeightProperty().bind(gridPane.heightProperty().subtract(50).divide(5));
+        imageView1.fitWidthProperty().bind(gridPane.widthProperty().subtract(10).divide(5));
+        imageView1.setPreserveRatio(true);
+        ImageView imageView2 = new ImageView(image2);
+        imageView2.fitHeightProperty().bind(gridPane.heightProperty().subtract(50).divide(5));
+        imageView2.fitWidthProperty().bind(gridPane.widthProperty().subtract(10).divide(5));
+        imageView2.setPreserveRatio(true);
         ImageView imageView3 = new ImageView(image3);
-        imageView3.setFitWidth(200);
-        imageView3.setFitHeight(200);
+        imageView3.fitHeightProperty().bind(gridPane.heightProperty().subtract(50).divide(5));
+        imageView3.fitWidthProperty().bind(gridPane.widthProperty().subtract(10).divide(5));
+        imageView3.setPreserveRatio(true);
+        ImageView imageView4 = new ImageView(image4);
+        imageView4.fitHeightProperty().bind(gridPane.heightProperty().subtract(50).divide(5));
+        imageView4.fitWidthProperty().bind(gridPane.widthProperty().subtract(10).divide(5));
+        imageView4.setPreserveRatio(true);
 
         // Add ImageViews to the GridPane
         gridPane.setGridLinesVisible(true);
         gridPane.add(imageView1, 0, 0);
-        gridPane.add(imageView2, 2, 1, 2, 2);
-        gridPane.add(imageView3, 0, 3);
+        gridPane.add(imageView2, 1, 0);
+        gridPane.add(imageView3, 2, 0);
+        gridPane.add(imageView4, 3, 0);
 
-        GridPane.setHalignment(imageView1, HPos.RIGHT);
+        GridPane.setHalignment(imageView1, HPos.CENTER);
         GridPane.setValignment(imageView1, VPos.CENTER);
 
         GridPane.setHalignment(imageView2, HPos.CENTER);
         GridPane.setValignment(imageView2, VPos.CENTER);
 
-        GridPane.setHalignment(imageView3, HPos.LEFT);
+        GridPane.setHalignment(imageView3, HPos.CENTER);
         GridPane.setValignment(imageView3, VPos.CENTER);
 
-        imageView2.setOnMousePressed(e -> {
-            System.out.println("Image2");
-        });
+        GridPane.setHalignment(imageView4, HPos.CENTER);
+        GridPane.setValignment(imageView4, VPos.CENTER);
+
 
     }
 }
