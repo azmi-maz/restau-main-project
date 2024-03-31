5066398ed3a3898a19579a07d0b1eb45f1633eaa
“azmi-maz” <adfadhli1991@gmail.com>
2024-03-31 04:27:04 +0100

    Added new navbar controller for Customer.

---
+++ src/main/java/org/group/project/controller/customer/CustomerOrderDetailsNavbarController.java
@@ -0,0 +1,36 @@
+package org.group.project.controller.customer;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.group.project.classes.ImageLoader;
+import org.group.project.scenes.CustomerMapsMain;
+import org.group.project.scenes.CustomerScenesMap;
+
+import java.net.URISyntaxException;
+
+public class CustomerOrderDetailsNavbarController {
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button returnToMenuButton;
+
+    public void initialize() throws URISyntaxException {
+
+        ImageLoader.setUpGraphicButton(homeButton, 25, 25, "home");
+
+        homeButton.setOnMousePressed(e -> {
+            CustomerScenesMap.getCustomerStage().setScene(CustomerScenesMap.getScenes().get(CustomerMapsMain.HOME));
+        });
+
+        ImageLoader.setUpGraphicButton(returnToMenuButton, 25, 25, "undo");
+
+        returnToMenuButton.setOnMousePressed(e -> {
+            CustomerScenesMap.getCustomerStage().setScene(CustomerScenesMap.getScenes().get(CustomerMapsMain.MENU));
+        });
+
+    }
+
+
+}
