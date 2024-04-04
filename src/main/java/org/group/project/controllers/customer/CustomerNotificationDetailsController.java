8899fd5ccbcce6d71f5c5097b7fd6524894bf582
“azmi-maz” <adfadhli1991@gmail.com>
2024-04-04 06:19:11 +0100

    Added controller and fxml for notification details page.

---
+++ src/main/java/org/group/project/controllers/customer/CustomerNotificationDetailsController.java
@@ -0,0 +1,61 @@
+package org.group.project.controllers.customer;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class CustomerNotificationDetailsController {
+
+    @FXML
+    private VBox vbox;
+
+    @FXML
+    private Label dateStampLabel;
+
+    @FXML
+    private Label subjectLabel;
+
+    @FXML
+    private TextArea notificationTextArea;
+
+    @FXML
+    private Button returnButton;
+
+    public void initialize() {
+
+        returnButton.setOnAction(e -> {
+            closeWindow();
+        });
+
+    }
+
+    public void populateNotificationDetails(
+            LocalDate notificationDate,
+            LocalTime notificationTime,
+            String notificationType,
+            String messageBody
+    ) {
+        dateStampLabel.setText(
+                "Date: " + notificationDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+                        + " " + notificationTime.format(DateTimeFormatter.ofPattern("HH:mm a"))
+        );
+
+        subjectLabel.setText(
+                "Subject: " + notificationType
+        );
+
+        notificationTextArea.setText(messageBody);
+    }
+
+    public void closeWindow() {
+        Stage stage = (Stage) vbox.getScene().getWindow();
+        stage.close();
+    }
+}
