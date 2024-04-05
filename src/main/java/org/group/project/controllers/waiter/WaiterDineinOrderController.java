963cf0cb92f4ca41c421c0933461973969309c1b
“azmi-maz” <adfadhli1991@gmail.com>
2024-04-05 03:11:21 +0100

    Added dinein order fxml and controller.

--- src/main/java/org/group/project/controllers/waiter/WaiterDineinOrderController.java
+++ src/main/java/org/group/project/controllers/waiter/WaiterDineinOrderController.java
@@ -1,4 +1,4 @@
-package org.group.project.controllers.customer;
+package org.group.project.controllers.waiter;
 
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
@@ -14,29 +14,18 @@ import javafx.scene.layout.*;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.group.project.Main;
-import org.group.project.classes.*;
+import org.group.project.classes.AlertPopUpWindow;
+import org.group.project.classes.FoodDrink;
+import org.group.project.classes.ImageLoader;
 import org.group.project.scenes.WindowSize;
-import org.group.project.scenes.customer.stackViews.MenuController;
-import org.group.project.scenes.customer.stackViews.OrderDetailsController;
-import org.group.project.test.generators.CustomerGenerator;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class CustomerOrderDetailsViewController {
-
-    @FXML
-    private ChoiceBox<String> choiceBox = new ChoiceBox<>();
-
-    @FXML
-    private Button confirmButton;
-
-    @FXML
-    private Button cancelButton;
+public class WaiterDineinOrderController {
 
     @FXML
     private BorderPane borderPane;
@@ -44,9 +33,8 @@ public class CustomerOrderDetailsViewController {
     @FXML
     private ImageView bgImage;
 
-    private List<FoodDrink> orderList;
-
-    private List<Order> newOrder;
+    @FXML
+    private ChoiceBox tableChoiceBox;
 
     @FXML
     private TableColumn<FoodDrink, String> noColumn;
@@ -70,17 +58,22 @@ public class CustomerOrderDetailsViewController {
     private TableColumn<FoodDrink, Button> actionButtonColumn2;
 
     @FXML
+    private Button newItemButton;
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    private Button cancelOrderButton;
+
+    private List<FoodDrink> orderList = new ArrayList<>();
+
+    @FXML
     private TableView<FoodDrink> orderDetailsTable =
             new TableView<>();
 
     private ObservableList<FoodDrink> data =
             FXCollections.observableArrayList();
-
-    public CustomerOrderDetailsViewController(List<FoodDrink> orderList,
-                                              List<Order> newOrder) {
-        this.orderList = orderList;
-        this.newOrder = newOrder;
-    }
 
     public void initialize() throws URISyntaxException {
 
@@ -97,10 +90,70 @@ public class CustomerOrderDetailsViewController {
                 BackgroundPosition.CENTER,
                 bSize)));
 
-        choiceBox.getItems().add("Delivery Order");
-        choiceBox.getItems().add("Takeaway Order");
+        // TODO enum or get from database
+        tableChoiceBox.getItems().add("Table A");
+        tableChoiceBox.getItems().add("Table B");
+        tableChoiceBox.getItems().add("Table C");
+        tableChoiceBox.getItems().add("Table D");
+        tableChoiceBox.getItems().add("Table E");
+        tableChoiceBox.getItems().add("Table F");
+        tableChoiceBox.getItems().add("Table G");
+        tableChoiceBox.getItems().add("Table H");
+        tableChoiceBox.getItems().add("Table I");
+        tableChoiceBox.getItems().add("Table J");
+        tableChoiceBox.getItems().add("Table K");
 
-        choiceBox.setValue("Delivery Order");
+        tableChoiceBox.setValue("Choose Table");
+
+        ImageLoader.setUpGraphicButton(newItemButton, 15, 15, "circle-plus");
+
+        newItemButton.setOnAction(e -> {
+
+            try {
+                FXMLLoader fxmlLoader =
+                        new FXMLLoader(Main.class.getResource(
+                                "smallwindows/add-dineinorder-item" +
+                                        ".fxml"));
+
+                VBox vbox = fxmlLoader.load();
+
+                WaiterDineinAddOrderController controller =
+                        fxmlLoader.getController();
+
+                controller.getCurrentOrderList(
+                        orderList
+                );
+
+                Scene editScene = new Scene(vbox,
+                        WindowSize.SMALL.WIDTH,
+                        WindowSize.SMALL.HEIGHT);
+
+                Stage editStage = new Stage();
+                editStage.setScene(editScene);
+                // TODO Should final variable this
+                editStage.setTitle("Add Order Item");
+
+                editStage.initModality(Modality.APPLICATION_MODAL);
+
+                editStage.showAndWait();
+
+                refreshOrderList();
+
+            } catch (IOException ex) {
+                // TODO catch error
+                throw new RuntimeException(ex);
+            }
+
+        });
+
+        confirmButton.setOnAction(e -> {
+
+
+        });
+
+        cancelOrderButton.setOnAction(e -> {
+            deleteAllOrder();
+        });
 
         noColumn.setText("No.");
         noColumn.setMinWidth(40);
@@ -134,37 +187,36 @@ public class CustomerOrderDetailsViewController {
 
         actionButtonColumn.setText("Action");
 
-        // TODO this doesn't work
-        actionButtonColumn1.setStyle("-fx-padding: 0; -fx-margin: 0; " +
-                "-fx-background-color: transparent; -fx-text-fill: transparent;");
-
-
         actionButtonColumn1.setMinWidth(35);
         actionButtonColumn1.setStyle("-fx-alignment: CENTER;");
         actionButtonColumn1.setCellValueFactory(cellData -> {
             Button editButton = new Button();
-
             ImageLoader.setUpGraphicButton(editButton, 15, 15, "edit");
-            String imageFile = cellData.getValue().getImageFileName();
-            String labelName = cellData.getValue().getItemNameForDisplay();
+            String itemName = cellData.getValue().getItemNameForDisplay();
+            String itemQuantity = String.valueOf(cellData.getValue().getQuantity());
 
-            editButton.setOnMousePressed(e -> {
+            editButton.setOnAction(e -> {
+
                 try {
                     FXMLLoader fxmlLoader =
                             new FXMLLoader(Main.class.getResource(
-                                    "smallwindows/edit-order-item" +
+                                    "smallwindows/edit-dineinorder-item" +
                                             ".fxml"));
 
-                    BorderPane borderPane = fxmlLoader.load();
+                    VBox vbox = fxmlLoader.load();
 
-                    CustomerMenuOrderEditItemController controller =
+                    WaiterDineinEditOrderController controller =
                             fxmlLoader.getController();
 
-                    controller.setItemToEdit("images/menu/" + imageFile,
-                            labelName, orderList);
-                    Scene editScene = new Scene(borderPane,
-                            WindowSize.MEDIUM.WIDTH,
-                            WindowSize.MEDIUM.HEIGHT);
+                    controller.setItemDetails(
+                            itemName,
+                            itemQuantity,
+                            orderList
+                    );
+
+                    Scene editScene = new Scene(vbox,
+                            WindowSize.SMALL.WIDTH,
+                            WindowSize.SMALL.HEIGHT);
 
                     Stage editStage = new Stage();
                     editStage.setScene(editScene);
@@ -180,8 +232,6 @@ public class CustomerOrderDetailsViewController {
                 } catch (IOException ex) {
                     // TODO catch error
                     throw new RuntimeException(ex);
-                } catch (URISyntaxException ex) {
-                    throw new RuntimeException(ex);
                 }
 
             });
@@ -189,49 +239,35 @@ public class CustomerOrderDetailsViewController {
             return new SimpleObjectProperty<>(editButton);
         });
 
-        actionButtonColumn2.setText("");
         actionButtonColumn2.setMinWidth(35);
         actionButtonColumn2.setStyle("-fx-alignment: CENTER;");
         actionButtonColumn2.setCellValueFactory(cellData -> {
             Button deleteButton = new Button();
-
             ImageLoader.setUpGraphicButton(deleteButton, 15, 15, "delete");
-            FoodDrink item = cellData.getValue();
+            String selectedItemName = cellData.getValue().getItemName();
+            FoodDrink selectedItem = cellData.getValue();
 
-            deleteButton.setOnMousePressed(e -> {
-                orderList.remove(item);
-                refreshOrderList();
+            deleteButton.setOnAction(e -> {
+
+                Optional<ButtonType> userChoice = promptForUserAcknowledgement(
+                        "Delete Item",
+                        "Do you want to delete " + selectedItemName + "?"
+                );
+
+                if (userChoice.get()
+                        .getButtonData().toString()
+                        .equalsIgnoreCase("OK_DONE")) {
+                    orderList.remove(selectedItem);
+                    refreshOrderList();
+                }
             });
 
             return new SimpleObjectProperty<>(deleteButton);
         });
 
-        confirmButton.setOnMousePressed(e -> {
-            newOrder.clear();
-            newOrder.add(createNewOrder());
-            OrderDetailsController.presenter.goToOrderConfirmation();
-        });
-
-        // TODO cancel button returns to menu with orderlist erased
-        cancelButton.setOnMousePressed(e -> {
-
-            Optional<ButtonType> userChoice = promptForUserAcknowledgement();
-
-            if (userChoice.get()
-                    .getButtonData().toString()
-                    .equalsIgnoreCase("OK_DONE")) {
-                cancelConfirmationAndGoBackToMenu();
-            }
-        });
-
         refreshOrderList();
-
-        // TODO to delete this if not in use
-//        addTableViewStyle();
-
     }
 
-    // TODO comment
     private void refreshOrderList() {
         orderDetailsTable.getItems().clear();
         data.clear();
@@ -239,69 +275,29 @@ public class CustomerOrderDetailsViewController {
         orderDetailsTable.setItems(data);
     }
 
-    // TODO this works but has warning errors
-    private void addTableViewStyle() {
-//        orderDetailsTable.skinProperty().addListener((a, b, newSkin) ->
-//        {
-//            TableView table = (TableView) orderDetailsTable.lookup(
-//                    "TableView");
-//
-//            System.out.println(table);
-//            System.out.println(table.getStylesheets().add(String.valueOf(Main.class.getResource("css/table-view" +
-//                ".css"))));
-//
-//
-//
-//        });
-    }
-
-    public Order createNewOrder() {
-        // TODO get user from main homepage, temp using dummy user
-        Customer newCustomer = CustomerGenerator.createCustomer1();
-
-        LocalDate dateNow = LocalDate.now();
-        LocalTime timeNow = LocalTime.now();
-
-        // TODO choicebox must not be empty
-        String orderType = choiceBox.getValue();
-        if (orderType.equalsIgnoreCase("delivery order")) {
-            orderType = "delivery";
-        } else {
-            orderType = "takeaway";
-        }
-
-        // TODO constant variables, where to final static
-        String orderStatus = "";
-        if (orderType.equalsIgnoreCase("delivery order")) {
-            orderStatus = "pending-approval";
-        } else {
-            orderStatus = "pending";
-        }
-
-        Order orderDetails = new Order(
-                newCustomer,
-                dateNow,
-                timeNow,
-                orderType,
-                orderStatus
-        );
-
-        for (FoodDrink item : orderList) {
-            orderDetails.addItemToOrderList(item);
-        }
-
-        return orderDetails;
-    }
-
-    public Optional<ButtonType> promptForUserAcknowledgement() {
+    public Optional<ButtonType> promptForUserAcknowledgement(
+            String header,
+            String message
+    ) {
         return AlertPopUpWindow.displayConfirmationWindow(
+                header,
+                message
+        );
+    }
+
+    public void deleteAllOrder() {
+
+        Optional<ButtonType> userChoice = promptForUserAcknowledgement(
                 "Cancel Order Request",
                 "Do you want to cancel this order?"
         );
+
+        if (userChoice.get()
+                .getButtonData().toString()
+                .equalsIgnoreCase("OK_DONE")) {
+            orderList.clear();
+            refreshOrderList();
+        }
     }
 
-    public void cancelConfirmationAndGoBackToMenu() {
-        OrderDetailsController.presenter.returnToMenu();
-        MenuController.orderList.clear();
-    }
 }
