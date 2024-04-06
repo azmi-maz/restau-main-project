b307f1a4cdf6dd42a2d64f48e7693044eff84b1c
“azmi-maz” <adfadhli1991@gmail.com>
2024-04-06 17:39:52 +0100

    Added Chef new item contructor + fxml - tested ok.

---
+++ src/main/java/org/group/project/controllers/chef/ChefAddNewItemController.java
@@ -0,0 +1,88 @@
+package org.group.project.controllers.chef;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.group.project.classes.DataManager;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ChefAddNewItemController {
+
+    @FXML
+    private VBox vbox;
+
+    @FXML
+    private TextField itemNameTextField;
+
+    @FXML
+    private ChoiceBox<String> itemTypeChoiceBox;
+
+    @FXML
+    private Button addNewItemButton;
+
+    @FXML
+    private Button cancelButton;
+
+    private List<String> menuItemList;
+
+    public void initialize() {
+
+        itemTypeChoiceBox.setValue("Choose type");
+        itemTypeChoiceBox.getItems().add("Food");
+        itemTypeChoiceBox.getItems().add("Drink");
+
+        addNewItemButton.setOnAction(e -> {
+
+            String itemName = itemNameTextField.getText().toLowerCase();
+            String itemType = itemTypeChoiceBox.getValue().toLowerCase();
+
+            List<String> newMenuItem = new ArrayList<>(Arrays.asList(
+                    itemName,
+                    itemType,
+                    "false"
+            ));
+
+            // TODO handle try catch
+            try {
+                DataManager.appendDataToFile("MENU", newMenuItem);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            // TODO comment why this needed
+            ChefMenuViewController controller =
+                    new ChefMenuViewController();
+
+            // TODO comment
+            try {
+                controller.refreshMenuItemList();
+            } catch (FileNotFoundException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            closeWindow();
+        });
+
+        cancelButton.setOnAction(e -> {
+            closeWindow();
+        });
+    }
+
+    public void setItemList(List<String> menuItemList) {
+        this.menuItemList = menuItemList;
+    }
+
+    private void closeWindow() {
+        Stage stage = (Stage) vbox.getScene().getWindow();
+        stage.close();
+    }
+
+}
