20f7cb022609549356a057eeb047d38c1fc03bc4
“azmi-maz” <adfadhli1991@gmail.com>
2024-04-07 08:58:50 +0100

    Wrap up the textarea for notification message.

--- src/main/java/org/group/project/controllers/customer/CustomerNotificationDetailsController.java
+++ src/main/java/org/group/project/controllers/customer/CustomerNotificationDetailsController.java
@@ -52,6 +52,7 @@ public class CustomerNotificationDetailsController {
         );
 
         notificationTextArea.setText(messageBody);
+        notificationTextArea.setWrapText(true);
     }
 
     public void closeWindow() {
