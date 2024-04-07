02f4a786caa7fa4841c47593b7ee3a5a670d9c8b
“azmi-maz” <adfadhli1991@gmail.com>
2024-04-07 08:55:48 +0100

    Implemented notifyAction method; Added methods to refactors methods used in other classes.

--- src/main/java/org/group/project/classes/Booking.java
+++ src/main/java/org/group/project/classes/Booking.java
@@ -1,5 +1,9 @@
 package org.group.project.classes;
 
+import javafx.collections.ObservableList;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -11,7 +15,7 @@ import java.util.List;
  *
  * @author azmi_maz
  */
-public class Booking {
+public class Booking implements NotifyAction {
     private final int bookingId;
     private final Customer customer;
     private final LocalDate bookingDate;
@@ -241,5 +245,139 @@ public class Booking {
                 + getEndBookingTime();
     }
 
+    // TODO comment
+    public String successfulBookingMessage() {
+        return String.format(
+                "Good news! Your table reservation "
+                        + "for %s at %s "
+                        + "is now confirmed.",
+                getBookingDateInFormat(),
+                getBookingTimeInFormat()
+        );
+    }
 
+    // TODO comment
+    public String failedBookingMessage() {
+        return String.format(
+                "We're terribly sorry! Unfortunately, your table reservation " +
+                        "for %s at %s is not successful.",
+                getBookingDateInFormat(),
+                getBookingTimeInFormat()
+        );
+    }
+
+    // TODO comment
+    public List<String> prepareNotificationData() throws FileNotFoundException {
+        List<String> data = new ArrayList<>();
+        data.add(String.valueOf(HelperMethods.getNewIdByFile("NOTIFICATION")));
+        data.add(String.valueOf(customer.getCustomerId()));
+        data.add(Notification.getNotificationDateForDatabase());
+        data.add(Notification.getNotificationTimeForDatabase());
+        data.add("booking");
+        data.add("false");
+        return data;
+    }
+
+    // TODO comment
+    public void approveBooking() throws IOException {
+        DataManager.editColumnDataByUniqueId("BOOKINGS",
+                bookingId, "bookingStatus",
+                "approved");
+    }
+
+    // TODO comment
+    public void cancelBooking() throws IOException {
+        DataManager.editColumnDataByUniqueId("BOOKINGS",
+                bookingId, "bookingStatus",
+                "failed");
+    }
+
+    public void deleteBooking() throws IOException {
+            DataManager.deleteUniqueIdFromFile("BOOKINGS",
+                    bookingId);
+    }
+
+    // TODO comment
+    public static void getUpdatedBookingData(
+            ObservableList<Booking> data
+    ) throws FileNotFoundException {
+
+        // TODO to filter based on userid for customer?
+        List<String> pendingTableReservations = DataManager.allDataFromFile("BOOKINGS");
+        for (String booking : pendingTableReservations) {
+            List<String> bookingDetails = List.of(booking.split(","));
+            int bookingId = Integer.parseInt(bookingDetails.get(0));
+            List<String> bookingDateDetails =
+                    List.of(bookingDetails.get(2).split("-"));
+            List<String> bookingTimeDetails =
+                    List.of(bookingDetails.get(3).split("-"));
+            Customer customer;
+            LocalDate bookingDate =
+                    LocalDate.of(Integer.parseInt(bookingDateDetails.get(0)),
+                            Integer.parseInt(bookingDateDetails.get(1)),
+                            Integer.parseInt(bookingDateDetails.get(2)));
+            LocalTime bookingTime =
+                    LocalTime.of(Integer.parseInt(bookingTimeDetails.get(0)),
+                            Integer.parseInt(bookingTimeDetails.get(1)));
+            int numOfGuests = Integer.parseInt(bookingDetails.get(4));
+            int bookingLength = Integer.parseInt(bookingDetails.get(5));
+            String[] bookingTables = bookingDetails.get(6).split(";");
+            String bookingStatus = bookingDetails.get(7);
+            List<Table> tablePreference = new ArrayList<>();
+
+            List<String> customerString = HelperMethods.getDataById("USERS",
+                    bookingDetails.get(DataFileStructure.getIndexByColName(
+                            "BOOKINGS", "userId")));
+            for (String rawTable : bookingTables) {
+                List<String> rawTableDetails = HelperMethods.getDataById(
+                        "TABLES", rawTable);
+                tablePreference.add(new Table(rawTableDetails.get(0),
+                        Integer.parseInt(rawTableDetails.get(1))));
+            }
+            if (customerString != null) {
+                customer = new Customer(
+                        customerString.get(DataFileStructure.getIndexByColName("USERS", "firstName")),
+                        customerString.get(DataFileStructure.getIndexByColName("USERS", "lastName")),
+                        customerString.get(DataFileStructure.getIndexByColName("USERS", "username")),
+                        Integer.parseInt(customerString.get(DataFileStructure.getIndexByColName("USERS", "userId"))),
+                        HelperMethods.formatAddressToRead(customerString.get(DataFileStructure.getIndexByColName("USERS", "address")))
+                );
+
+                // TODO make sure pending-approval status is standardized
+                if (bookingStatus.equalsIgnoreCase("pending-approval")) {
+                    data.add(new Booking(
+                            bookingId,
+                            customer,
+                            bookingDate,
+                            bookingTime,
+                            numOfGuests,
+                            tablePreference,
+                            bookingLength,
+                            bookingStatus
+                    ));
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void notifyCustomer(Customer customer,
+                               boolean isSuccessfulRequest) throws FileNotFoundException {
+        List<String> newNotificationMessage = prepareNotificationData();
+
+        if (isSuccessfulRequest) {
+            newNotificationMessage.add(successfulBookingMessage());
+        } else {
+            newNotificationMessage.add(failedBookingMessage());
+        }
+
+        // TODO try catch
+        try {
+            DataManager.appendDataToFile("NOTIFICATION", newNotificationMessage);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
