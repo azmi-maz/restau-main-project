4066fedfbff8e0d09f749443e649b2c987693289
“azmi-maz” <adfadhli1991@gmail.com>
2024-04-08 07:24:42 +0100

    Added the static controller for BookingView.

--- src/main/java/org/group/project/scenes/customer/mainViews/BookingsView.java
+++ src/main/java/org/group/project/scenes/customer/mainViews/BookingsView.java
@@ -2,8 +2,10 @@ package org.group.project.scenes.customer.mainViews;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.group.project.Main;
+import org.group.project.controllers.customer.CustomerBookingsHistoryViewController;
 import org.group.project.mapscenes.view.ViewMaker;
 import org.group.project.scenes.WindowSize;
 
@@ -12,6 +14,8 @@ import java.io.IOException;
 public class BookingsView implements ViewMaker {
 
     private Stage stage;
+
+    public static CustomerBookingsHistoryViewController controller;
 
     public BookingsView(Stage stage) {
 
@@ -23,7 +27,12 @@ public class BookingsView implements ViewMaker {
 
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(
                 "customerscenes/mapscenes/customer-viewbookings.fxml"));
-        return new Scene(fxmlLoader.load(), WindowSize.MAIN.WIDTH,
+
+        VBox vbox = fxmlLoader.load();
+
+        controller = fxmlLoader.getController();
+
+        return new Scene(vbox, WindowSize.MAIN.WIDTH,
                 WindowSize.MAIN.HEIGHT);
     }
 }
