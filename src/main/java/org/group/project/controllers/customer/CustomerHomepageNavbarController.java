bff9bcf008059b3daffc8d6ee55856167de2a9ca
“azmi-maz” <adfadhli1991@gmail.com>
2025-03-29 03:51:11 +0800

    Modified the Customer homepage navbar to view booking table first instead of booking history.

--- src/main/java/org/group/project/controllers/customer/CustomerHomepageNavbarController.java
+++ src/main/java/org/group/project/controllers/customer/CustomerHomepageNavbarController.java
@@ -14,6 +14,7 @@ import org.group.project.scenes.customer.CustomerScenesMap;
 import org.group.project.scenes.customer.mainViews.BookingsView;
 import org.group.project.scenes.customer.mainViews.NotificationView;
 import org.group.project.scenes.customer.mainViews.OrdersView;
+import org.group.project.scenes.customer.mainViews.TablesView;
 
 /**
  * This class loads up the customer home page navigation bar.
@@ -21,123 +22,122 @@ import org.group.project.scenes.customer.mainViews.OrdersView;
  * @author azmi_maz
  */
 public class CustomerHomepageNavbarController {
-    private static final String MENU_BUTTON = "menu";
-    private static final String RESERVATION_BUTTON = "reservation";
-    private static final String HISTORY_BUTTON = "history";
-    private static final String NOTIFICATION_BUTTON = "notification";
-    private static final String HELP_BUTTON = "help";
-    private static final String SETTINGS_BUTTON = "settings";
-    private static final String USER_BUTTON = "user";
-    private static final String POWER_BUTTON = "power";
-    private static final int BUTTON_WIDTH = 25;
-    private static final int BUTTON_HEIGHT = 25;
-    private static final String ACTIVE_USER = "ACTIVE_USER";
-    @FXML
-    private Button menuButton;
+        private static final String MENU_BUTTON = "menu";
+        private static final String RESERVATION_BUTTON = "reservation";
+        private static final String HISTORY_BUTTON = "history";
+        private static final String NOTIFICATION_BUTTON = "notification";
+        private static final String HELP_BUTTON = "help";
+        private static final String SETTINGS_BUTTON = "settings";
+        private static final String USER_BUTTON = "user";
+        private static final String POWER_BUTTON = "power";
+        private static final int BUTTON_WIDTH = 25;
+        private static final int BUTTON_HEIGHT = 25;
+        private static final String ACTIVE_USER = "ACTIVE_USER";
+        @FXML
+        private Button menuButton;
 
-    @FXML
-    private Button reservationButton;
+        @FXML
+        private Button reservationButton;
 
-    @FXML
-    private Button historyButton;
+        @FXML
+        private Button historyButton;
 
-    @FXML
-    private Button notificationButton;
+        @FXML
+        private Button notificationButton;
 
-    @FXML
-    private Button helpButton;
+        @FXML
+        private Button helpButton;
 
-    @FXML
-    private Button settingButton;
+        @FXML
+        private Button settingButton;
 
-    @FXML
-    private Button userButton;
+        @FXML
+        private Button userButton;
 
-    @FXML
-    private Button logOffButton;
+        @FXML
+        private Button logOffButton;
 
-    /**
-     * This initializes the controller for the fxml.
-     */
-    public void initialize() {
+        /**
+         * This initializes the controller for the fxml.
+         */
+        public void initialize() {
 
-        ImageLoader.setUpGraphicButton(menuButton,
-                BUTTON_WIDTH, BUTTON_HEIGHT, MENU_BUTTON);
+                ImageLoader.setUpGraphicButton(menuButton,
+                                BUTTON_WIDTH, BUTTON_HEIGHT, MENU_BUTTON);
 
-        ImageLoader.setUpGraphicButton(reservationButton,
-                BUTTON_WIDTH, BUTTON_HEIGHT, RESERVATION_BUTTON);
+                ImageLoader.setUpGraphicButton(reservationButton,
+                                BUTTON_WIDTH, BUTTON_HEIGHT, RESERVATION_BUTTON);
 
-        ImageLoader.setUpGraphicButton(historyButton,
-                BUTTON_WIDTH, BUTTON_HEIGHT, HISTORY_BUTTON);
+                ImageLoader.setUpGraphicButton(historyButton,
+                                BUTTON_WIDTH, BUTTON_HEIGHT, HISTORY_BUTTON);
 
-        ImageLoader.setUpGraphicButton(notificationButton,
-                BUTTON_WIDTH, BUTTON_HEIGHT, NOTIFICATION_BUTTON);
+                ImageLoader.setUpGraphicButton(notificationButton,
+                                BUTTON_WIDTH, BUTTON_HEIGHT, NOTIFICATION_BUTTON);
 
-        ImageLoader.setUpGraphicButton(helpButton,
-                BUTTON_WIDTH, BUTTON_HEIGHT, HELP_BUTTON);
+                ImageLoader.setUpGraphicButton(helpButton,
+                                BUTTON_WIDTH, BUTTON_HEIGHT, HELP_BUTTON);
 
-        ImageLoader.setUpGraphicButton(settingButton,
-                BUTTON_WIDTH, BUTTON_HEIGHT, SETTINGS_BUTTON);
+                ImageLoader.setUpGraphicButton(settingButton,
+                                BUTTON_WIDTH, BUTTON_HEIGHT, SETTINGS_BUTTON);
 
-        ImageLoader.setUpGraphicButton(userButton,
-                BUTTON_WIDTH, BUTTON_HEIGHT, USER_BUTTON);
+                ImageLoader.setUpGraphicButton(userButton,
+                                BUTTON_WIDTH, BUTTON_HEIGHT, USER_BUTTON);
 
-        ImageLoader.setUpGraphicButton(logOffButton,
-                BUTTON_WIDTH, BUTTON_HEIGHT, POWER_BUTTON);
+                ImageLoader.setUpGraphicButton(logOffButton,
+                                BUTTON_WIDTH, BUTTON_HEIGHT, POWER_BUTTON);
 
-        menuButton.setOnMousePressed(e -> {
-            CustomerScenesMap.getCustomerStage().setScene(
-                    CustomerScenesMap.getScenes()
-                            .get(CustomerMapsMain.MENU));
-        });
+                menuButton.setOnMousePressed(e -> {
+                        CustomerScenesMap.getCustomerStage().setScene(
+                                        CustomerScenesMap.getScenes()
+                                                        .get(CustomerMapsMain.MENU));
+                });
 
-        reservationButton.setOnMousePressed(e -> {
-            BookingsView.controller.refreshReservationList();
-            CustomerScenesMap.getCustomerStage()
-                    .setScene(CustomerScenesMap.getScenes()
-                            .get(CustomerMapsMain.BOOKING));
-        });
+                reservationButton.setOnMousePressed(e -> {
+                        CustomerScenesMap.getCustomerStage()
+                                        .setScene(CustomerScenesMap.getScenes()
+                                                        .get(CustomerMapsMain.TABLES));
+                        TablesView.controller.resetsBookingForm();
+                });
 
-        historyButton.setOnMousePressed(e -> {
-            OrdersView.controller.refreshOrderHistoryList();
-            CustomerScenesMap.getCustomerStage()
-                    .setScene(CustomerScenesMap.getScenes()
-                            .get(CustomerMapsMain.ORDER));
-        });
+                historyButton.setOnMousePressed(e -> {
+                        OrdersView.controller.refreshOrderHistoryList();
+                        CustomerScenesMap.getCustomerStage()
+                                        .setScene(CustomerScenesMap.getScenes()
+                                                        .get(CustomerMapsMain.ORDER));
+                });
 
-        notificationButton.setOnMousePressed(e -> {
-            NotificationView.controller.refreshNotificationList();
-            CustomerScenesMap.getCustomerStage()
-                    .setScene(CustomerScenesMap.getScenes()
-                            .get(CustomerMapsMain.NOTIFICATION));
-        });
+                notificationButton.setOnMousePressed(e -> {
+                        NotificationView.controller.refreshNotificationList();
+                        CustomerScenesMap.getCustomerStage()
+                                        .setScene(CustomerScenesMap.getScenes()
+                                                        .get(CustomerMapsMain.NOTIFICATION));
+                });
 
-        helpButton.setOnMousePressed(e -> {
-            // Purposely not implemented
-        });
+                helpButton.setOnMousePressed(e -> {
+                        // Purposely not implemented
+                });
 
-        settingButton.setOnMousePressed(e -> {
-            // Purposely not implemented
-        });
+                settingButton.setOnMousePressed(e -> {
+                        // Purposely not implemented
+                });
 
-        userButton.setOnMousePressed(e -> {
-            UserProfileView userProfileView = new UserProfileView();
-            userProfileView.showWindow();
-        });
+                userButton.setOnMousePressed(e -> {
+                        UserProfileView userProfileView = new UserProfileView();
+                        userProfileView.showWindow();
+                });
 
-        logOffButton.setOnMousePressed(e -> {
-            // Log off by removing active user info
-            try {
-                DataManager.clearFileData(ACTIVE_USER);
-            } catch (ClearFileFailedException ex) {
-                AlertPopUpWindow.displayErrorWindow(
-                        ex.getMessage()
-                );
-                ex.printStackTrace();
-            }
-            MainScenesMap.getStage().setScene(MainScenesMap.getScenes()
-                    .get(MainScenes.LOGIN));
-        });
+                logOffButton.setOnMousePressed(e -> {
+                        // Log off by removing active user info
+                        try {
+                                DataManager.clearFileData(ACTIVE_USER);
+                        } catch (ClearFileFailedException ex) {
+                                AlertPopUpWindow.displayErrorWindow(
+                                                ex.getMessage());
+                                ex.printStackTrace();
+                        }
+                        MainScenesMap.getStage().setScene(MainScenesMap.getScenes()
+                                        .get(MainScenes.LOGIN));
+                });
 
-    }
+        }
 }
