df37bc25c71e1c9cdcce829bd8f549cd7cfed5cb
“azmi-maz” <adfadhli1991@gmail.com>
2025-03-29 03:50:31 +0800

    Added the booking view table navbar controller and fxml.

---
+++ src/main/java/org/group/project/controllers/customer/CustomerBookingTablesNavbarController.java
@@ -0,0 +1,57 @@
+package org.group.project.controllers.customer;
+
+import org.group.project.classes.auxiliary.ImageLoader;
+import org.group.project.scenes.customer.CustomerMapsMain;
+import org.group.project.scenes.customer.CustomerScenesMap;
+import org.group.project.scenes.customer.mainViews.BookingsView;
+import org.group.project.scenes.customer.mainViews.TablesView;
+import org.group.project.scenes.main.CustomerView;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class CustomerBookingTablesNavbarController {
+
+        private static final String UNDO_BUTTON = "undo";
+        private static final String RESERVATION_BUTTON = "history";
+        private static final int BUTTON_WIDTH = 25;
+        private static final int BUTTON_HEIGHT = 25;
+
+        @FXML
+        private Button returnButton;
+
+        @FXML
+        private Button bookingsHistoryButton;
+
+        /**
+         * This initializes the controller for the fxml.
+         */
+        public void initialize() {
+
+                ImageLoader.setUpGraphicButton(returnButton,
+                                BUTTON_WIDTH, BUTTON_HEIGHT, UNDO_BUTTON);
+
+                ImageLoader.setUpGraphicButton(bookingsHistoryButton,
+                                BUTTON_WIDTH, BUTTON_HEIGHT, RESERVATION_BUTTON);
+
+                returnButton.setOnMousePressed(e -> {
+                        CustomerView.customerNotificationNavbarController
+                                        .refreshNotificationCounter();
+                        CustomerScenesMap.getCustomerStage().setScene(
+                                        CustomerScenesMap.getScenes()
+                                                        .get(CustomerMapsMain.HOME));
+                        TablesView.controller.resetsBookingForm();
+                        TablesView.controller.clearGrid();
+                });
+
+                bookingsHistoryButton.setOnMousePressed(e -> {
+                        BookingsView.controller.refreshReservationList();
+                        CustomerScenesMap.getCustomerStage().setScene(
+                                        CustomerScenesMap.getScenes()
+                                                        .get(CustomerMapsMain.BOOKING));
+                });
+
+        }
+
+}
